e6e9aacaa875508d30766c1bfd28b11ae677aa6d
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-22 01:40:54 +0200

    Creating getCompetitions

--- app/Http/Controllers/CompetitionController.php
+++ app/Http/Controllers/CompetitionController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class CompetitionController extends Controller
 {
-    //
+    public function getCompetitions(){
+        $competitions = Competition::all();
+        if(!$competitions){
+            return response()->json([
+                'message' => 'No Competitions found'
+            ],404);
+        }
+        return response()->json([
+            'message' => 'Successfully fetched all Competitions',
+            'competitions' => $competitions
+        ],200);
+    } 
 }
