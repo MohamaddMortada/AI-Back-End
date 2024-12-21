7f3111f1ea5c191198ee7679ae9ee70ef4dab1d2
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-22 01:08:49 +0200

    Creating getEvent

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -18,4 +18,17 @@ class EventController extends Controller
             'events' => $events
         ],200);
     } 
+
+    public function getEvent($id){
+        $event = Event::find($id);
+        if(!$event){
+            return response()->json([
+                'message' => 'Event not found'
+            ],404);
+        }
+        return response()->json([
+            'message' => 'Event found successfully',
+            'event' => $event
+        ],200);
+    }
 }
