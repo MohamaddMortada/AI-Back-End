2e2d9fd5ed6cb77af434f37541227c53bf927442
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-22 00:28:38 +0200

    Creating deleteUser

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -88,4 +88,21 @@ class UsersController extends Controller
             'user' => $user
         ], 200);  
     }
+
+    public function deleteUser($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found'
+            ], 404); 
+        }
+
+        $user->delete();
+        
+        return response()->json([
+            'message' => 'User deleted successfully'
+        ], 200);  
+    }
 }
