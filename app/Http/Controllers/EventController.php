3c89bde1beaeef01931caa5f0feba2e63b6795cc
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-22 01:06:24 +0200

    Creating getEvents

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function getEvents(){
+        $events = Event::all();
+        if(!$events){
+            return response()->json([
+                'message' => 'No events found'
+            ],404);
+        }
+        return response()->json([
+            'message' => 'Successfully fetched all events',
+            'events' => $events
+        ],200);
+    } 
 }
