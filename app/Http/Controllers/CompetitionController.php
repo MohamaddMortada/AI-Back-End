9219fb54bcf83a232b36514a73b6ebc9e4cfdd64
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-22 01:44:32 +0200

    Creating getCompetition

--- app/Http/Controllers/CompetitionController.php
+++ app/Http/Controllers/CompetitionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Competition;
 
 class CompetitionController extends Controller
 {
@@ -18,4 +19,17 @@ class CompetitionController extends Controller
             'competitions' => $competitions
         ],200);
     } 
+
+    public function getCompetition($id){
+        $competition = competition::find($id);
+        if(!$competition){
+            return response()->json([
+                'message' => 'competition not found'
+            ],404);
+        }
+        return response()->json([
+            'message' => 'competition found successfully',
+            'competition' => $competition
+        ],200);
+    }
 }
