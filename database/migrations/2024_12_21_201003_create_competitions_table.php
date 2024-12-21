9684ba4539991aa96bab7a650f5283d6ad44325b
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-21 22:10:44 +0200

    Fixing Competitions table

--- database/migrations/2024_12_21_201003_create_competitions_table.php
+++ database/migrations/2024_12_21_201003_create_competitions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('compititions', function (Blueprint $table) {
+        Schema::create('competitions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->date('date');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('compititions');
+        Schema::dropIfExists('competitions');
     }
 };
