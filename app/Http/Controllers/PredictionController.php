b600004d4d35096326c1e9f8ca0997ed30ffe253
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-22 15:28:32 +0200

    Creating getPredictions

--- app/Http/Controllers/PredictionController.php
+++ app/Http/Controllers/PredictionController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class PredictionController extends Controller
 {
-    //
+    public function getPredictions(){
+        $predictions = Prediction::all();
+        if(!$predictions){
+            return response()->json([
+                'message' => 'No predictions found'
+            ],404);
+        }
+        return response()->json([
+            'message' => 'Successfully fetched all predictions',
+            'predictions' => $predictions
+        ],200);
+    }
 }
