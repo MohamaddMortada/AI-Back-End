26b84c93c30322a16257fc8517d8c22b935a8f6e
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-22 17:53:24 +0200

    Fixing bug in StatisticsController

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Statistics;
 
-class StatisticskController extends Controller
+class StatisticsController extends Controller
 {
     public function getStatistics(){
         $statistics = Statistics::all();
