f5429fcff908605c5acdb487450fcc12c6f9c8ff
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-22 15:33:12 +0200

    Creating setPrediction

--- app/Http/Controllers/PredictionController.php
+++ app/Http/Controllers/PredictionController.php
@@ -31,4 +31,24 @@ class PredictionController extends Controller
             'prediction' => $prediction
         ],200);
     }
+
+    public function setprediction(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'confidence' => 'required|integer',
+            'user_id' => 'required|exists:users,id'
+        ]);
+
+        $prediction = prediction::create([
+            'name' => $request->input('name'),
+            'confidence' => $request->input('confidence'),
+            'user_id' => $request->input('user_id'),
+        ]);
+        if(!$prediction)   
+            return response()->json(['message' => 'Error while creating prediction'], 500);
+        return response()->json([
+            'message' => 'prediction created successfully',
+            'prediction' => $prediction
+        ],201);
+    }
 }
