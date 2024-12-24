2e2f8b638e23e205557ad3235664958f8356d54e
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-24 21:01:51 +0200

    Adjusting API routes

--- routes/api.php
+++ routes/api.php
@@ -12,14 +12,19 @@ use App\Http\Controllers\Athlete_EventController;
 use App\Http\Controllers\AuthController;
  
 Route::group([
-    'middleware' => 'api',
     'prefix' => 'auth'
-], function ($router) {
+], function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
-    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
-    Route::post('/me', [AuthController::class, 'me'])->middleware('auth:api')->name('me');
+});
+
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'auth'
+], function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::get('/me', [AuthController::class, 'me'])->name('me');
 });
 
 Route::get('/users', [UsersController::class,'getUsers']);
