a09659871de0f19edeedb4dafcfe9b6192e6585f
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-27 13:42:17 +0200

    Implementing RightLeg Angle

--- app/PythonScripts/body_detect.py
+++ app/PythonScripts/body_detect.py
@@ -54,6 +54,17 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                            tuple(np.multiply(elbowRight, [640, 480]).astype(int)), 
                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
                                 )
+            hipRight = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            kneeRight = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+            ankleRight = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+            
+            angleRightLeg = calculate_angle(hipRight, kneeRight, ankleRight)
+            
+            cv2.putText(image, str(angleRightLeg), 
+                           tuple(np.multiply(hipRight, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            
                        
         except:
             pass
