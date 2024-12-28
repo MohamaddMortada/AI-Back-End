c78ee85bd3b68f2c2af14e8fb834375eabeef331
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-28 21:29:36 +0200

    Implementing calculate_angle Method

--- app/PythonScripts/image_detect.py
+++ app/PythonScripts/image_detect.py
@@ -8,6 +8,19 @@ app = Flask(__name__)
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
+def calculate_angle(a, b, c):
+    a = np.array(a) 
+    b = np.array(b) 
+    c = np.array(c) 
+    
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+    
+    if angle > 180.0:
+        angle = 360 - angle
+        
+    return angle 
+
 
 def detect_pose():
     if 'image' not in request.files:
