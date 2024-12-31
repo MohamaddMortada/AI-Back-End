ef2a8275e09ebb904526ec4cbd113aa4b34b10ab
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-31 15:13:32 +0200

    Creating detectImage Method

--- app/Http/Controllers/DetectController.php
+++ app/Http/Controllers/DetectController.php
@@ -11,19 +11,33 @@ class DetectController extends Controller {
     public function detectImage(Request $request)
     {
         $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', 
+            'image' => 'required|file|mimes:jpeg,png,jpg|max:2048',
         ]);
 
-        if ($request->hasFile('image')) {
+        try {
             $file = $request->file('image');
 
-            $path = $file->store('temp_images', 'public');
+            $response = Http::attach(
+                'image', file_get_contents($file), $file->getClientOriginalName()
+            )->post('http://10.0.2.2:5000/detect_image');
 
-            $storedImage = Storage::disk('public')->get($path);
-
-            return response($storedImage, 200)->header('Content-Type', $file->getMimeType());
+            if ($response->successful()) {
+                return response()->json([
+                    'message' => 'Image processed successfully.',
+                    'detection_result' => $response->json(),
+                ]);
+            } else {
+                return response()->json([
+                    'error' => 'Failed to detect the image.',
+                    'status' => $response->status(),
+                ], $response->status());
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'An error occurred while processing the image.',
+                'details' => $e->getMessage(),
+            ], 500);
         }
-
-        return response()->json(['message' => 'Image upload failed!'], 400);
     }
+
 }
