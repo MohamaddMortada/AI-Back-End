54c28ee237677579ab8e182bd00b9cebbd61aa67
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-05 16:27:51 +0200

    Craeting  events.json

--- app/Http/Controllers/EventDetectorController.php
+++ app/Http/Controllers/EventDetectorController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Services\EventService;
 
-class EventDetectorController extends Controller
-{
-    //
+class EventDetectorController extends Controller {
+    protected $eventService;
+
+    public function __construct(EventService $eventService)
+    {
+        $this->eventService = $eventService;
+    }
+
+    public function showEventId($name, $type, $gender)
+    {
+        $eventId = $this->eventService->getEventId($name, $type, $gender);
+        if ($eventId !== null) {
+            return response()->json(['event_id' => $eventId]);
+        } else {
+            return response()->json(['error' => 'Event not found'], 404);
+        }
+    }
 }
