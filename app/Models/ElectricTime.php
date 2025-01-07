5e8bc9ff8eba785d62d8a8bd18da92797c9c1f6e
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-07 21:47:29 +0200

    Creatimg ElectricTime Model

---
+++ app/Models/ElectricTime.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class ElectricTime extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['sync_key', 'start_time', 'stop_time', 'user_a_id', 'user_b_id'];
+}
