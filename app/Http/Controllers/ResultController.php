1c30569e2449a25d3766250c25c4e99b11a4c838
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-10 17:03:15 +0200

    Creating getResult

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -6,12 +6,25 @@ use Illuminate\Http\Request;
 
 class ResultController extends Controller
 {
-    public function getResults()
-    {
+    public function getResults() {
         $results = Result::all();
         return response()->json([
             'message' => 'Successfully fetched all results',
             'results' => $results,
+        ], 200);
+    }
+    public function getResult($id) {
+        $result = Result::find($id);
+
+        if (!$result) {
+            return response()->json([
+                'message' => 'Result not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'message' => 'Result fetched successfully',
+            'result' => $result,
         ], 200);
     }
 }
