45dacf5349444723d38231e9858894c6b54397c8
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-10 17:01:37 +0200

    Creating Result model

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Result extends Model
 {
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'result'];
     
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
